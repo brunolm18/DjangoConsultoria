023a89fbaf8b015da2ea3af6e49f63e75bc22e74
BrunoLezama <brunolezamamendez18@gmail.com>
2025-06-27 15:11:46 -0600

    actualizacion de base de datos

--- DjangoConsultoria/settings.py
+++ DjangoConsultoria/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'DjangoConsultoria.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600)
+    'default': dj_database_url.config(env=("POSTGRESQL_URL"), conn_max_age=600,ssl_require=True)
     
 }
 
