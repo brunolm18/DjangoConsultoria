89e0c4116a79cd0cca06e32635fcc95cf97a2f26
BrunoLezama <brunolezamamendez18@gmail.com>
2025-06-27 15:28:17 -0600

    cambio en allowedhost

--- DjangoConsultoria/settings.py
+++ DjangoConsultoria/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host]
 
 
 
