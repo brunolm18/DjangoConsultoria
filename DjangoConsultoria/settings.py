23b7911f1dd8b22c516e344a66286f1a910955c3
BrunoLezama <brunolezamamendez18@gmail.com>
2025-06-27 14:35:09 -0600

    actualizacion de settings,requirements.txt

--- DjangoConsultoria/settings.py
+++ DjangoConsultoria/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
+    'storages',
     
 ]
 
@@ -123,11 +124,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
+# tests.py
 from django.test import TestCase
+from .models import Author, Post
 
-# Create your tests here.
+class BlogSimpleTests(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(
+            full_name="Test Author",
+            email="author@test.com",
+            biography="Simple bio",
+            country="Testland"
+        )
+        self.post = Post.objects.create(
+            author=self.author,
+            title="Test Post",
+            description="Test Description",
+            text="Some test text"
+        )
+
+    def test_author_str(self):
+        self.assertEqual(str(self.author), "author@test.com")
+
+    def test_post_str(self):
+        self.assertEqual(str(self.post), "Test Post by Test Author")
