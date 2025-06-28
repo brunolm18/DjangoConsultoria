57808941f21b1cb81f2c76b1f6f56b3e85984440
BrunoLezama <brunolezamamendez18@gmail.com>
2025-06-27 20:54:52 -0600

    añadiendo context_data

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import DetailView,ListView
 from .models import Author,Post,Categoria
@@ -16,6 +17,12 @@ class PostDetail(DetailView):
     queryset = Post.objects.all()
     context_object_name = 'post'
     template_name = 'blog/blog_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Categoria.objects.all()
+        return context
+
 
 
 
