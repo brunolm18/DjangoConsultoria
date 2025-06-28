507a66003c12cd5c92c4a60d8b18bebef8e1c289
BrunoLezama <brunolezamamendez18@gmail.com>
2025-06-28 15:59:31 -0600

    cambio en settings.py

--- DjangoConsultoria/settings.py
+++ DjangoConsultoria/settings.py
@@ -146,9 +146,7 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_ADDRESSING_STYLE = "auto"
-    AWS_QUERYSTRING_AUTH = False
+    
 
     STORAGES = {
         "default": {
